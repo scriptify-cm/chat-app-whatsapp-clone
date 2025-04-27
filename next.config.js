0b727e6bfb428c435e81aef911e0eef8114c1101
Brightky Efoo <brightefoo@gmail.com>
2025-04-27 14:25:18 +0100

    chore: update Next.js configuration
    - Removed 'output' and 'distDir' properties from next.config.js to streamline the build process.
    - Set 'unoptimized' property for images to false, enhancing image optimization for better performance.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
   images: {
-    unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
